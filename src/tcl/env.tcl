61ca07774d86eb3a03c9599a5d447a388907ba8e
Neel <neelgala@gmail.com>
2018-10-15 19:11:55 +0530

    fixes for vivado tcl scripts.

--- src/tcl/env.tcl
+++ src/tcl/env.tcl
@@ -8,11 +8,11 @@ global ip_project
 set home_dir $::env(SHAKTI_HOME)
 set fpga_dir $home_dir/fpga
 set ip_project_dir $fpga_dir/manage_ip
-set core_project_dir $fpga_dir/e-class
+set core_project_dir $fpga_dir/c-class
 
 # set ip project name
 set ip_project manage_ip
-set core_project e-class
+set core_project c-class
 puts "home_dir: $home_dir"
 puts "fpga_dir: $fpga_dir"
 puts "ip_project_dir: $ip_project_dir"
