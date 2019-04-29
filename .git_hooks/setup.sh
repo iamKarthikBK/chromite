7fbcc2e089babd3f4cd00011312199a576f2e203
Anmol Sahoo <anmol.sahoo25@gmail.com>
2019-04-29 12:25:15 +0530

    updated hooks

--- .git_hooks/setup.sh
+++ .git_hooks/setup.sh
@@ -10,3 +10,14 @@ echo ""
 echo "Setting up post-commit"
 ln -s -f ../../.git_hooks/post-commit ./.git/hooks/post-commit
 chmod a+x ./.git/hooks/post-commit
+echo "Done"
+
+echo "Setting up post-merge"
+ln -s -f ../../.git_hooks/post-merge ./.git/hooks/post-merge
+chmod a+x ./.git/hooks/post-merge
+echo "Done"
+
+echo "Setting up post-checkout"
+ln -s -f ../../.git_hooks/post-checkout ./.git/hooks/post-checkout
+chmod a+x ./.git/hooks/post-checkout
+echo "Done"
