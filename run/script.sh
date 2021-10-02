c2e906077addb47590d828d739dafd62d00b7a73
B K Karthik <bkkarthik@pesu.pes.edu>
2021-10-02 15:51:05 +0530

    run: script.sh now looks at run/ instead of the parent directory
    Signed-off-by: B K Karthik <bkkarthik@pesu.pes.edu>

--- run/script.sh
+++ run/script.sh
@@ -16,32 +16,32 @@ make generate_boot_files -j12
 
 zip -rv config$1_build build
 
-cat ../template.c ../matmult-int.c > benchmarks/hello/hello.c
+cat run/template.c run/matmult-int.c > benchmarks/hello/hello.c
 echo "Hello World"
 make hello
 cat bin/app_log >> $LOG
 
-cat ../template.c ../matmult-int.c > benchmarks/hello/hello.c
+cat run/template.c run/matmult-int.c > benchmarks/hello/hello.c
 echo "MATMULT-INT"
 make hello
 cat bin/app_log >> $LOG
 
-cat ../template.c ../minver.c > benchmarks/hello/hello.c
+cat run/template.c run/minver.c > benchmarks/hello/hello.c
 echo "MINVER"
 make hello 
 cat bin/app_log | tail -2 >> $LOG
 
-cat ../template.c ../aha-mont64.c > benchmarks/hello/hello.c
+cat run/template.c run/aha-mont64.c > benchmarks/hello/hello.c
 echo "MONT64"
 make hello
 cat bin/app_log >> $LOG
 
-cat ../template.c ../crc32.c > benchmarks/hello/hello.c
+cat run/template.c run/crc32.c > benchmarks/hello/hello.c
 echo "CRC32"
 make hello 
 cat bin/app_log >> $LOG
 
-cat ../template.c ../aes.c > benchmarks/hello/hello.c
+cat run/template.c run/aes.c > benchmarks/hello/hello.c
 echo "AES256"
 make hello 
 cat bin/app_log >> $LOG
